117f646315b870cc3214ec2ab51ff44894db7a4f
Eike Spielberg <Eike.Spielberg@uni-due.de>
2019-02-20 10:49:26 +0100

    adding sql queries to budget calculator

--- app.py
+++ app.py
@@ -12,18 +12,18 @@ from query.QueryExecutor import QueryExecutor
 from text_analyzer.TextAnalyzer import TextAnalyzer
 
 app = Flask(__name__)
-
 # define configuration file
 app.config.from_envvar("LIBINTEL_SETTINGS")
+
 your_rest_server_port = 5000
 
 
 # eureka client setup
-try:
-    eureka_client.init_registry_client(eureka_server="http://localhost:8761/eureka", app_name="scripts",
-                                       instance_port=your_rest_server_port)
-except URLError:
-    print('Could not connect to eureka server')
+# try:
+#     eureka_client.init_registry_client(eureka_server="http://localhost:8761/eureka", app_name="scripts",
+#                                        instance_port=your_rest_server_port)
+# except URLError:
+#     print('Could not connect to eureka server')
 
 # define root directory of application
 builder = NetworkBuilder(app.config.get("LIBINTEL_DATA_DIR"),
@@ -33,22 +33,37 @@ builder = NetworkBuilder(app.config.get("LIBINTEL_DATA_DIR"),
                          app.config.get("LIBINTEL_NEO4J_PASSWORD"))
 
 
-connection = psycopg2.connect(database=app.config.get("LIBINTEL_DATASOURCE_DBNAME"),
-                              host=app.config.get("LIBINTEL_DATASOURCE_HOST"),
-                              port=app.config.get("LIBINTEL_DATASOURCE_PORT"),
-                              user=app.config.get("LIBINTEL_DATASOURCE_USERNAME"),
-                              password=app.config.get("LIBINTEL_DATASOURCE_PASSWORD")
+finance_connection = psycopg2.connect(database=app.config.get("LIBINTEL_FINANCE_DATASOURCE_DBNAME"),
+                              host=app.config.get("LIBINTEL_FINANCE_DATASOURCE_HOST"),
+                              port=app.config.get("LIBINTEL_FINANCE_DATASOURCE_PORT"),
+                              user=app.config.get("LIBINTEL_FINANCE_DATASOURCE_USERNAME"),
+                              password=app.config.get("LIBINTEL_FINANCE_DATASOURCE_PASSWORD")
                               )
 
+
+data_connection = psycopg2.connect(database=app.config.get("LIBINTEL_DATA_DATASOURCE_DBNAME"),
+                              host=app.config.get("LIBINTEL_DATA_DATASOURCE_HOST"),
+                              port=app.config.get("LIBINTEL_DATA_DATASOURCE_PORT"),
+                              user=app.config.get("LIBINTEL_DATA_DATASOURCE_USERNAME"),
+                              password=app.config.get("LIBINTEL_DATA_DATASOURCE_PASSWORD")
+                              )
+
+
+bibliometrics_connection = psycopg2.connect(database=app.config.get("LIBINTEL_BIBLIOMETRICS_DATASOURCE_DBNAME"),
+                              host=app.config.get("LIBINTEL_BIBLIOMETRICS_DATASOURCE_HOST"),
+                              port=app.config.get("LIBINTEL_BIBLIOMETRICS_DATASOURCE_PORT"),
+                              user=app.config.get("LIBINTEL_BIBLIOMETRICS_DATASOURCE_USERNAME"),
+                              password=app.config.get("LIBINTEL_BIBLIOMETRICS_DATASOURCE_PASSWORD")
+                              )
 
 query_executor = QueryExecutor(app.config.get("ALTMETRIC_API_KEY"),
                                app.config.get("LIBINTEL_USER_EMAIL"),
                                app.config.get("LIBINTEL_DATA_DIR"),
-                               connection)
+                               bibliometrics_connection)
 
-text_analyzer = TextAnalyzer(app.config.get("LIBINTEL_DATA_DIR"), connection)
+text_analyzer = TextAnalyzer(app.config.get("LIBINTEL_DATA_DIR"), bibliometrics_connection)
 
-budget_calculator = BudgetCalculator(app.config.get("LIBINTEL_DATA_DIR"), connection,
+budget_calculator = BudgetCalculator(app.config.get("LIBINTEL_DATA_DIR"), finance_connection,
                                      app.config.get("LIBINTEL_USAGE_URL"))
 
 
@@ -98,32 +113,39 @@ def restart_query_execution(query_id):
     query_executor.collect_data('restart')
 
 
-@app.route("/calculateTextrank/<query_id>", method=['POST'])
+@app.route("/calculateTextrank/<query_id>", methods=['POST'])
 def calculate_textrank(query_id):
     text_analyzer.initalize_analyzer(query_id)
     text_analyzer.calculate_text_rank()
 
 
-@app.route("/prepareDataForML/<query_id>", method=['POST'])
-def calculate_textrank(query_id):
+@app.route("/prepareDataForML/<query_id>", methods=['POST'])
+def prepare_data_for_ml(query_id):
     text_analyzer.initalize_analyzer(query_id)
     text_analyzer.prepare_machine_learning_data()
 
 
-@app.route("/getEids/<query_id>", method=['GET'])
+@app.route("/getEids/<query_id>", methods=['GET'])
 def get_eids(query_id):
     query_executor.initalize_query(query_id)
     return query_executor.read_eids('')
 
 
-@app.route("/getMissedEids/<query_id>", method=['POST'])
+@app.route("/getMissedEids/<query_id>", methods=['POST'])
 def get_missed_eids(query_id):
     query_executor.initalize_query(query_id)
     return query_executor.read_eids('restart')
 
 
-@app.route("calculate_costs_for collection/<collection>", method=['POST'])
-def calculate_usage_fraction_for_collection_and_year(collection, year):
+@app.route("/prepare_collection_lists/<year>")
+def generate_collection_lists_for_year(year):
+    budget_calculator.generate_collection_lists(year)
+    return 'done'
+
+
+@app.route("/calculate_costs_for collection/<collection>", methods=['POST'])
+def calculate_usage_fraction_for_collection_and_year(collection, year, total_costs):
     budget_calculator.set_journal_collection(collection, year)
     budget_calculator.read_issn_list()
-    budget_calculator.analyze_usage()
+    budget_calculator.analyze_usage(total_costs)
+    return 'done'
--- budget_calculator/BudgetCalculator.py
+++ budget_calculator/BudgetCalculator.py
@@ -1,7 +1,11 @@
 import requests
 
-sql_insert = "INSERT INTO usage_fraction (issn, year, fraction_usage) VALUES (%s, %s, %s);"
-
+sql_insert = "INSERT INTO usage_fraction (issn, year, fraction_usage) VALUES (%s, %f, %f);"
+sql_add_subject = "UPDATE usage_fraction SET subject = ezb_list.fach FROM ezb_list WHERE ezb_list.e_issn = usage_fraction.issn;"
+sql_get_all = "SELECT * FROM usage_fraction;"
+sql_insert_fractional_costs = "INSERT INTO fractional_costs (issn, subject, fractional_cost) VALUES (%s, %s, %f);"
+sql_get_all_anchors = "SELECT distinct anchor FROM ezb_list ORDER BY anchor;"
+sql_get_issns_for_anchor = "SELECT e_issn FROM ezb_list WHERE anchor = %s;"
 
 class BudgetCalculator:
 
@@ -13,6 +17,8 @@ class BudgetCalculator:
         self.file = ''
         self.data_url = data_url
         self.year = 0
+        self.usage_fraction = {}
+        self.cost_per_issn = {}
 
     def set_journal_collection(self, journal_collection, year):
         self.journal_collection = journal_collection
@@ -34,7 +40,7 @@ class BudgetCalculator:
             else:
                 self.issn_set.add(line)
 
-    def analyze_usage(self):
+    def analyze_usage(self, total_costs):
         total_usage = 0
         usages = {}
         for issn in self.issn_set:
@@ -44,11 +50,60 @@ class BudgetCalculator:
                 usage = r.json()['totalRequests']
                 usages[issn] = usage
                 total_usage = total_usage + usage
-        usage_fraction = {}
         cursor = self.connection.cursor()
         for issn in self.issn_set:
-            usage_fraction[issn] = usages[issn] / total_usage
-            cursor.execute(sql_insert, (issn, self.year, usage_fraction))
+            usage_fraction = usages[issn] / total_usage
+            cost_per_issn = usage_fraction * total_costs
+            cursor.execute(sql_insert, (issn.trim(), self.year, usage_fraction, cost_per_issn))
             self.connection.commit()
+        cursor.execute(sql_add_subject)
         cursor.close()
-        return usage_fraction
+        return 'done'
+
+    def distribute_costs(self):
+        cursor = self.connection.cursor()
+        cursor.execute(sql_get_all)
+        for row in cursor.fetchall():
+            issn = row[0]
+            fractional_costs = row[2]
+            subjects_field = row[3]
+            if subjects_field is None:
+                print('could not distirbute costs (' + str(fractional_costs) + ') for ' + issn)
+                continue
+            if ";" in subjects_field:
+                subjects = subjects_field.split(";")
+                parts = len(subjects)
+                for subject in subjects:
+                    cursor.execute(sql_insert_fractional_costs, (issn, subject, fractional_costs / parts))
+            else:
+                cursor.execute(sql_insert_fractional_costs, (issn, subjects_field, fractional_costs))
+        cursor.close()
+
+    def generate_collection_lists(self, year):
+        cursor = self.connection.cursor()
+        cursor.execute(sql_get_all_anchors)
+        for anchor_found in cursor.fetchall():
+            if anchor_found[0] is None:
+                continue
+            anchor = anchor_found[0].replace("?", "maybe")
+
+            cursor.execute(sql_get_issns_for_anchor, (anchor,))
+            issns_list = cursor.fetchall()
+            print("found " + str(len(issns_list)) + " issns in anchcr " + anchor)
+            if issns_list is None:
+                print('issns is None for collection ' + anchor)
+                continue
+            if len(issns_list) == 0:
+                print('empty issn list for collection ' + anchor)
+                continue
+            filename = self.upload_dir + '/budgetcalculator/' + str(year) + '_' + anchor + '.txt'
+            with open(filename, 'w') as list_file:
+                for issn_tuple in issns_list:
+                    issn = issn_tuple[0]
+                    if issn is None:
+                        continue
+                    if issn == '':
+                        continue
+                    for issn_ind in issn.split(";"):
+                        list_file.write(issn_ind + '\n')
+                list_file.close()
